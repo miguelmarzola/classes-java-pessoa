c00ec091a0812bf09641cee81208999296df3c9b
miguel marzola <miguel.7884187@aluno.mg.gov.br>
2026-04-10 10:35:52 -0300

    Create Individuo.java

---
+++ Individuo.java
@@ -0,0 +1,12 @@
+package Miguel;
+public class Individuo {
+String nome;
+int idade;
+
+    public  void caminhar() {
+        System.out.println("caminhando...");
+    }
+    public void dormir() {
+            System.out.println("dormindo...");
+    }
+}
